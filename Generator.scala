47822ca5e06c2b975866ca1e6c9e2f51d3e8226a
Rok Kralj <info@rok-kralj.net>
2016-10-30 20:20:56 +0000

    Improve style in Generator.scala

--- Generator.scala
+++ Generator.scala
@@ -1,14 +1,12 @@
 import schemer.Schemer
 
-object Generator {
-	def main(args: Array[String]) {
-		println(args.lift(0) match {
-			case Some(file) => {
-				val schemer = new Schemer(file)
-				val tableName = args.lift(1).getOrElse("data")
-				schemer.table(tableName)
-			}
-			case None => "USAGE: java Generator sample.json [table_name]"
-		})
-	}
+object Generator extends App {
+	println(args match {
+		case Array(file) =>
+			new Schemer(file).table("data")
+		case Array(file, tableName) =>
+			new Schemer(file).table(tableName)
+		case _ =>
+			"USAGE: java Generator sample.json [table_name]"
+	})
 }
